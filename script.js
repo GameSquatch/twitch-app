0d5196a25f006f1745859e5caa13cdac0eb8aa40
unknown <broncoscorpio@gmail.com>
2018-05-08 19:07:22 -0600

    Display info from twitch API call.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //https://wind-bow.gomix.me/twitch-api/channels/
-var channelIDs = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var channelIDs = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "OverwatchLeague"];
 var channelsURL = "https://wind-bow.gomix.me/twitch-api/channels/";
 var streamsURL = "https://wind-bow.gomix.me/twitch-api/streams/";
 var content;
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
     channelIDs.forEach(function(id) {
         var url = channelsURL + id + "?callback=?";
-        var html = "<div class='row align-items-center'><div class='col'>";
+        var html = "<div class='row align-items-center channel'>" + column;
         var image = "<img width='60' height='60' src='";
 
         $.getJSON(url, function(data) {
@@ -24,11 +24,30 @@ $(document).ready(function() {
                 html += data.game + "</div>";
                 html += column;
                 html += data.views + "</div>";
-                html += "</div>";
-                content.append(html);
-            }
-        });
+                //html += "</div>";
+                //content.append(html);
 
-    });
+                url = streamsURL + id + "?callback=?";
+                $.getJSON(url, function(data) {
+                    
+                    html += column;
+                    console.log(data);
+
+                    if (data.stream && data.stream !== null) {
+                        html += "STREAMING</div>";
+                    } else {
+                        html += "NO</div>";
+                    }
+
+                    html += "</div>";//closes row div
+                    content.append(html);
+
+                });//END getJSON
+
+            }//ENDIF
+
+        });//END getJSON
+        
+    });//END FOREACH
 
 });
