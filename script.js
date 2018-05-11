15f7549f7f3be1ddd83e0b9bd9fcbdb266e109e2
unknown <broncoscorpio@gmail.com>
2018-05-11 09:03:50 -0600

    Add JennaJulien as a channel.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //https://wind-bow.gomix.me/twitch-api/channels/
-var channelIDs = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "OverwatchLeague"];
+var channelIDs = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "OverwatchLeague", "JennaJulien"];
 var channelsURL = "https://wind-bow.gomix.me/twitch-api/channels/";
 var streamsURL = "https://wind-bow.gomix.me/twitch-api/streams/";
 var content;
