f7c26b751d34bcff75d635b1a28cf23f50f0844a
Ezekiel Williams <broncoscorpio@gmail.com>
2018-05-11 21:08:48 -0600

    Add actual content information to collapse area.

--- script.js
+++ script.js
@@ -14,35 +14,59 @@ $(document).ready(function() {
         var html = "<div class='row align-items-center channel'><div class='col-2'>";
         var image = "<img src='";
 
-        $.getJSON(url, function(data) {//channels
-            console.log(data);
+        $.getJSON(url, function(data1) {//channels
+            console.log(data1);
             
-            if (!data.error) {
-                html += image + data.logo + "'></div>";
+            if (!data1.error) {
+                html += image + data1.logo + "'></div>";
                 html += "<div class='col-auto'>";
-                html += data.name + "</div>";
+                html += "<a href='" + data1.url + "' target='_blank'>" + data1.display_name + "</a></div>";
 
                 url = streamsURL + id + "?callback=?";
-                $.getJSON(url, function(data) {//streams
+                $.getJSON(url, function(data2) {//streams
                     
                     html += "<div class='col'>";
-                    console.log(data);
+                    console.log(data2);
 
-                    if (data.stream && data.stream !== null) {
+                    if (data2.stream && data2.stream !== null) {
                         html += "<span class='live light float-right'></span></div>";
                     } else {
                         html += "<span class='offline light float-right'></span></div>";
                     }
                     html += "</div>";
 
-                    html += "<div class='row text-center'><div class='col-12'>";
+                    html += "<div class='row text-center moreInfo'><div class='col-12'>";
                     html += "<div><i class='fas fa-caret-down' role='button' data-toggle='collapse' data-target='#box" + i + "'></i></div>";
                     html += "</div>";//close column
 
-                    html += "<div class='collapse text-left' id='box" + i + "'>This will have additional info about the channel soon.</div>";
-   
+                    html += "<div class='collapse text-left' id='box" + i + "'>";
+                    if (data2.stream !== null) {
+                        html += "<p>Stream type: " + data2.stream.stream_type + "</p>";
+                        html += "<p>Stream game: " + data2.stream.game + "</p>";
+                        html += "<p>Viewers: " + data2.stream.viewers + "</p>";
+                        html += "<p>Avg FPS: " + Math.round(data2.stream.average_fps) + "</p>";
+                    }
+                    if (data1.status !== null)
+                        html += "<p>Status: " + data1.status + "</p>";
+                    else
+                        html += "<p>Status: NONE</p>";
+                    html += "<p>Followers: " + data1.followers + "</p>";
+                    html += "<p>Views: " + data1.views + "</p>";
+                    if (data1.game !== null)
+                        html += "<p>Game: " + data1.game + "</p>";
+                    else
+                        html += "<p>Game: NONE</p>";
+                    
+                    html += "</div>";
                     html += "</div>";//close collapse row and start row
-                    content.append(html);
+
+                    if (data2.stream !== null) {
+                        content.prepend(html);
+                    }
+                    else {
+                        content.append(html);
+                    }
+                    
 
                     i++;
                 });//END getJSON streams
